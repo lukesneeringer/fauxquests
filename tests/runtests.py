3c8116ebcbbbfea218c70e8a0352f25d55d32e6a
Luke Sneeringer <luke@sneeringer.com>
2013-10-23 17:55:16 -0600

    Add a YML file for Travis to use for testing.

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,11 +1,11 @@
-from fauxquests.compat import unittest
 import os
 import sys
-
 
 # Ensure that the torch directory is part of our Python path.
 APP_ROOT = os.path.realpath(os.path.dirname(__file__) + '/../')
 sys.path.append(APP_ROOT)
+
+from fauxquests.compat import unittest
 
 # Find tests.
 def load_tests(loader, standard_tests, throwaway):
